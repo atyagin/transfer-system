da3bd992754bdd27f67687ef25a38ba9c350ce6a
ilya <ilyaman5@gmail.com>
2020-08-20 17:10:08 +0700

    add logs

--- src/main/java/com/ilya/transfer_system/MoneyTransferSystemApplication.java
+++ src/main/java/com/ilya/transfer_system/MoneyTransferSystemApplication.java
@@ -1,13 +1,30 @@
 package com.ilya.transfer_system;
 
+import com.ilya.transfer_system.service.TransferService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneyTransferSystemApplication {
+public class MoneyTransferSystemApplication implements ApplicationRunner {
+
+    @Autowired
+    private TransferService transferService;
+
+    public static Logger LOG = LoggerFactory
+            .getLogger(MoneyTransferSystemApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyTransferSystemApplication.class, args);
+        LOG.info("APPLICATION FINISHED");
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        transferService.start();
+    }
 }
